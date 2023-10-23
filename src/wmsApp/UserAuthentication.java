9b7a8655e652186ed35c6340b706a38ca8dd8ec8
gaspar-dylan <dylan07@my.yorku.ca>
2023-10-23 18:48:08 -0400

    user authentication backend

--- src/wmsApp/UserAuthentication.java
+++ src/wmsApp/UserAuthentication.java
@@ -1,31 +1,28 @@
 package wmsApp;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class UserAuthentication {
+	
+	protected String username;
+	protected String password;
+	protected boolean authentication;
 
-    public static void main(String[] args) {
-        // Simulate user authentication
-        if (authenticateUser()) {
-            System.out.println("Authentication successful. You can access the system.");
-        } else {
-            System.out.println("Authentication failed. Access denied.");
-        }
-    }
-
-    // Method to validate user credentials
-    private static boolean authenticateUser() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter username: ");
-        String username = scanner.nextLine();
-        System.out.print("Enter password: ");
-        String password = scanner.nextLine();
-
-        // Replace this with actual user database validation logic
-        // For simplicity, we'll use hard-coded credentials for demonstration purposes
-        if ("admin".equals(username) && "password123".equals(password)) {
-            return true;
-        } else {
-            return false;
-        }
+	public UserAuthentication(String username, String password) throws IOException {
+		this.username = username;
+		this.password = password;
+		this.authentication = authenticateUser(username, password);
+	}
+	
+	
+	private static boolean authenticateUser(String username, String password) throws IOException {
+    	AdminDB admin_database = new AdminDB();
+    	
+    	if (admin_database.credentials.containsKey(username)) {
+    		String correctPassword = admin_database.credentials.get(username);
+    		if(correctPassword.equals(password)) {
+    			return true;
+    		}	
+    	}
+    	return false;
     }
 }
